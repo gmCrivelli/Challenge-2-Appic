654ab2c41bfc9de4c1776972f9485dcd1f3450ff
Gustavo De Mello Crivelli <gmcrivelli@gmail.com>
2017-07-03 14:16:52 -0300

    Movement changed to pan gesture with motion for fine tuning, shoots on click and also on play button press

--- teamAppic/teamAppic/GameScene.swift
+++ teamAppic/teamAppic/GameScene.swift
@@ -11,7 +11,7 @@ import GameplayKit
 import GameController
 import QuartzCore
 
-class GameScene: SKScene, ReactToMotionEvents {
+class GameScene: SKScene, ReactToMotionEvents, UIGestureRecognizerDelegate {
     
     var targetController : TargetController!
     var gameNode = SKNode()
@@ -20,6 +20,16 @@ class GameScene: SKScene, ReactToMotionEvents {
     var maxX:CGFloat!
     var maxY:CGFloat!
     var currentAimPosition:CGPoint = CGPoint(x: 0.0, y: 0.0)
+    
+    private var prevTilt : CGPoint = CGPoint.zero
+    private var currentTilt : CGPoint = CGPoint.zero
+    private var prevTouch : CGPoint!
+    private var panVel : CGPoint = CGPoint(x: 0.0, y: 0.0)
+    private var tiltVel : CGPoint = CGPoint(x: 0.0, y: 0.0)
+    private var needsUpdatePan : Bool = false
+    private let dampening:CGFloat = 0.87
+    
+    private let motionMovement:CGFloat = 100
     
     override func didMove(to view: SKView) {
         
@@ -30,7 +40,6 @@ class GameScene: SKScene, ReactToMotionEvents {
         aimNode = gameNode.childNode(withName: "aim") as! SKSpriteNode
         targetController = TargetController(screenSize: self.size, gameNode: gameNode)
 
-        // 
         let targetNode1 = targetController.addTarget(typeOfNode: "shape")
         targetController.moveBetweenSides(node: targetNode1)
         
@@ -41,36 +50,84 @@ class GameScene: SKScene, ReactToMotionEvents {
         maxY = self.size.height/2
         
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.selectTapped(_:)))
-        let pressType = UIPressType.select
-        tapGestureRecognizer.allowedPressTypes = [NSNumber(value: pressType.rawValue)];
+        tapGestureRecognizer.allowedPressTypes = [NSNumber(value: UIPressType.playPause.rawValue),NSNumber(value: UIPressType.select.rawValue)];
+        tapGestureRecognizer.delegate = self
         self.view?.addGestureRecognizer(tapGestureRecognizer)
         
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(self.panAim(_:)))
+        panGestureRecognizer.delegate = self
+        self.view?.addGestureRecognizer(panGestureRecognizer)
     }
     
     func selectTapped(_ sender: UITapGestureRecognizer) {
         targetController.detectHit(aimNode.position)
     }
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        aimNode.removeAllActions()
-        //aimNode.run(SKAction.move(to: currentAimPosition, duration: ))
+    func panAim(_ sender: UIPanGestureRecognizer) {
+        switch sender.state {
+        case .began:
+            prevTilt = currentTilt
+            needsUpdatePan = false
+        case .ended:
+            needsUpdatePan = true
+        default:
+            break
+        }
         
-        aimNode.position = currentAimPosition
+        print(sender.velocity(in: self.view))
+        panVel = sender.velocity(in: self.view)
+        
+        panVel.x = panVel.x * (1 + abs(panVel.x * 0.0003)) * 0.001
+        panVel.y = panVel.y * (1 + abs(panVel.y * 0.0003)) * 0.003
     }
     
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        if (gestureRecognizer is UIPanGestureRecognizer || gestureRecognizer is UITapGestureRecognizer) {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+        self.aimNode.position.x = max(min(self.aimNode.position.x + panVel.x + tiltVel.x * motionMovement, maxX), -maxX)
+        self.aimNode.position.y = max(min(self.aimNode.position.y - panVel.y - tiltVel.y * motionMovement, maxY), -maxY)
+        
+        tiltVel = CGPoint.zero
+        //if needsUpdatePan {
+        
+        if panVel.x * panVel.x + panVel.y * panVel.y < 1 {
+            needsUpdatePan = false
+            panVel = CGPoint.zero
+        }
+        panVel.x = panVel.x * dampening
+        panVel.y = panVel.y * dampening
+        
+        /*}
+        else{
+            panVel = CGPoint.zero
+        }*/
+    }
     
     func motionUpdate(motion: GCMotion) {
         // Called whenever there is a change in the controller motion
         
         /// Motion Config
-        let x = motion.gravity.x
-        let y = -motion.gravity.y
-        let p_x:CGFloat = 0.7
+        //let x:CGFloat = CGFloat(motion.gravity.x)
+        //let y:CGFloat = -(CGFloat)(motion.gravity.y)
+        let p_x:CGFloat = 0.6
         let p_y:CGFloat = 0.35
+        let g_x:CGFloat = CGFloat(motion.gravity.x) //min(max(CGFloat(motion.gravity.x), -p_x), p_x) / p_x
+        let g_y:CGFloat = CGFloat(motion.gravity.y) //min(max(CGFloat(motion.gravity.y), -p_y), p_y) / p_y
         
-        currentAimPosition = CGPoint(x: min(p_x,max(-p_x,CGFloat(x))) / p_x * maxX,
-                              y: min(p_y,max(-p_y,CGFloat(y))) / p_y * maxY)
+        //currentAimPosition = CGPoint(x: min(p_x,max(-p_x,CGFloat(x))) / p_x * maxX, y: min(p_y,max(-p_y,CGFloat(y))) / p_y * maxY)
+        
+        tiltVel.x = g_x - prevTilt.x
+        tiltVel.y = g_y - prevTilt.y
+        prevTilt = CGPoint(x: g_x, y: g_y)
+        currentTilt = prevTilt
+        
         
         /// Swipe Config
         //let x = motion.controller?.microGamepad?.dpad.xAxis.value
@@ -78,6 +135,7 @@ class GameScene: SKScene, ReactToMotionEvents {
         //currentAimPosition = (CGFloat(x!) * maxX, min(0.5,max(-0.5,CGFloat(y!))) * 2 * maxY)
         
         //currentAimPosition = (CGFloat() * maxX, min(0.5,max(-0.5,CGFloat())) * 2 * maxY)
-        //print(motion.gravity.x, motion.gravity.y)
+        //print(motion.gravity)
+        //print(motion.userAcceleration)
     }
 }
